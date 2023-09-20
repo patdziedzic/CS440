e0b0a122a3b0dacdee8a66fdc6654cde82373a4a
patdziedzic <danieldziedzic93@gmail.com>
2023-09-19 20:34:40 -0400

    Basic hello world program

---
+++ proj1/src/sourcecode/Cell.java
@@ -0,0 +1,21 @@
+package sourcecode;
+
+public class Cell {
+    //REGARDING OPENNESS
+    private int numOpenNeighbors; //increment for each new open neighbor
+    private boolean canOpen; //if numOpenNeighbors > 1, make false
+    private boolean isOpen; //initialized false
+
+    //REGARDING FLAMMABILITY
+    private boolean onFire; //initialized false
+    private double flammability; //flammability of the cell based on neighbors, initially 0
+
+
+    public Cell() {
+        this.numOpenNeighbors = 0;
+        this.canOpen = true;
+        this.isOpen = false;
+        this.onFire = false;
+        flammability = 0;
+    }
+}
--- proj1/src/sourcecode/Main.java
+++ proj1/src/sourcecode/Main.java
@@ -1,6 +1,46 @@
 package sourcecode;
 
 public class Main {
+    public static final int DIM = 100;
+
+
+    //the ship layout - 2D array of Cell objects
+    public static Cell[][] ship = new Cell[DIM][DIM];
+
+
+
+
+
+
+    /**
+     * Get random int between min to max
+     * @return randomly generated int
+     */
+    private static int rand(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
+    /**
+     * Get random double between min to max
+     * @return randomly generated double
+     */
+    private static double rand(double min, double max) {
+        return ((Math.random() * (max - min)) + min);
+    }
+
+
+
+
+
+
+
+    private static void makeShip() {
+        //ship is a DIM x DIM 2D array of Cell objects
+
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
