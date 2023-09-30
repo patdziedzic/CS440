d9f3e19e33562d521a20990eaab61d1f4a01a481
patdziedzic <danieldziedzic93@gmail.com>
2023-09-30 13:48:10 -0400

    9/30 morning - make ship

--- proj1/src/sourcecode/Cell.java
+++ proj1/src/sourcecode/Cell.java
@@ -1,6 +1,10 @@
 package sourcecode;
 
 public class Cell {
+    //COORDINATES
+    private int row;
+    private int col;
+
     //REGARDING OPENNESS
     private int numOpenNeighbors; //increment for each new open neighbor
     private boolean canOpen; //if numOpenNeighbors > 1, make false
@@ -9,6 +13,7 @@ public class Cell {
     //REGARDING FLAMMABILITY
     private boolean onFire; //initialized false
     private double flammability; //flammability of the cell based on neighbors, initially 0
+    private int fireNeighbors; //K
 
     //Default Cell Constructor
     public Cell() {
@@ -18,4 +23,83 @@ public class Cell {
         this.onFire = false;
         this.flammability = 0;
     }
+
+    //Cell Constructor given row and col
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+
+        this.numOpenNeighbors = 0;
+        this.canOpen = true;
+        this.isOpen = false;
+        this.onFire = false;
+        this.flammability = 0;
+    }
+
+    public int getNumOpenNeighbors() {
+        return numOpenNeighbors;
+    }
+
+    public void incNumOpenNeighbors() {
+        this.numOpenNeighbors++;
+        if (numOpenNeighbors > 1){
+            canOpen = false;
+        }
+    }
+
+    public boolean getCanOpen() {
+        return canOpen;
+    }
+
+    public void setCanOpen(boolean canOpen) {
+        this.canOpen = canOpen;
+    }
+
+    public boolean getIsOpen() {
+        return isOpen;
+    }
+
+    public void setIsOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public boolean getOnFire() {
+        return onFire;
+    }
+
+    public void setOnFire(boolean onFire) {
+        this.onFire = onFire;
+    }
+
+    public double getFlammability() {
+        return flammability;
+    }
+
+    public void setFlammability(double flammability) {
+        this.flammability = flammability;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+/*
+    private void changeNeighborK(int row, int col) {
+        try {
+
+        } catch (ArrayIndexOutOfBoundsException ignore) { }
+    }
+     */
 }
--- proj1/src/sourcecode/Main.java
+++ proj1/src/sourcecode/Main.java
@@ -1,11 +1,15 @@
 package sourcecode;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static final int DIM = 100;
+    public static final int D = 5;
+    public static final double q = 0.5;
 
 
     //the ship layout - 2D array of Cell objects
-    public static Cell[][] ship = new Cell[DIM][DIM];
+    public static Cell[][] ship;
+    private static Cell initial;
 
 
 
@@ -35,13 +39,179 @@ public class Main {
 
 
     private static void makeShip() {
-        //ship is a DIM x DIM 2D array of Cell objects
+        //ship is a D x D 2D array of Cell objects
+        int row = rand(0, D-1);
+        int col = rand(0, D-1);
 
+        //open the initial cell
+        initial = openCell(ship[row][col]);
+        printShip();
+
+        openBlockedCandidates();
+        //openDeadEnds();
+    }
+
+    private static void openBlockedCandidates() {
+
+        //populating blocked candidates
+        ArrayList<Cell> blockedCandidates = new ArrayList<>(); //blocked cells with one open neighbor
+        for (int r = 0; r < D; r++) {
+            for (int c = 0; c < D; c++) {
+                /*if (!ship[r][c].getIsOpen() && ship[r][c].getNumOpenNeighbors() == 1) {
+                    blockedCandidates.add(ship[r][c]);
+                }*/
+                addCandidate(ship[r][c], blockedCandidates);
+            }
+        }
+
+        //opening blocked candidates
+        while (!blockedCandidates.isEmpty()) {
+            int randomIndex = rand(0, blockedCandidates.size() - 1);
+            Cell randomBlocked = blockedCandidates.get(randomIndex);
+            openCell(randomBlocked);
+            printShip();
+
+            //Check U/D/L/R if it is now a candidate, if it is then add it
+            //up
+            try {
+                addCandidate(ship[randomBlocked.getRow() - 1][randomBlocked.getCol()], blockedCandidates);
+            } catch (ArrayIndexOutOfBoundsException ignore) { }
+            //down
+            try {
+                addCandidate(ship[randomBlocked.getRow() + 1][randomBlocked.getCol()], blockedCandidates);
+            } catch (ArrayIndexOutOfBoundsException ignore) { }
+            //left
+            try {
+                addCandidate(ship[randomBlocked.getRow()][randomBlocked.getCol() - 1], blockedCandidates);
+            } catch (ArrayIndexOutOfBoundsException ignore) { }
+            //right
+            try {
+                addCandidate(ship[randomBlocked.getRow()][randomBlocked.getCol() + 1], blockedCandidates);
+            } catch (ArrayIndexOutOfBoundsException ignore) { }
+
+            updateArrayList(blockedCandidates);
+        }
+    }
+
+    private static void addCandidate(Cell cell, ArrayList<Cell> a) {
+        if (!cell.getIsOpen() && cell.getNumOpenNeighbors() == 1) {
+            a.add(cell);
+        }
+    }
+
+    private static void openDeadEnds() {
+        //handling dead ends
+        ArrayList<Cell> deadEnds = new ArrayList<>();
+        for (int r = 0; r < D; r++) {
+            for (int c = 0; c < D; c++) {
+                if (ship[r][c].getIsOpen() && ship[r][c].getNumOpenNeighbors() == 1) {
+                    deadEnds.add(ship[r][c]);
+                }
+            }
+        }
+
+        int threshold = deadEnds.size() / 2;
+
+        //opening dead ends
+        while (deadEnds.size() > threshold) {
+            int randomIndex = rand(0, deadEnds.size()-1);
+            Cell randomDeadEnd = deadEnds.get(randomIndex);
+            openCell(randomDeadEnd);
+
+            updateArrayList(deadEnds);
+        }
+
+        //get in an arraylist all cells that are open and have 1 open neighbors
+    }
+
+
+    /**
+     * Updates blockedCandidates or deadEnds
+     */
+    private static void updateArrayList(ArrayList<Cell> a) {
+        for (int i = 0; i < a.size(); i++) {
+            Cell c = a.get(i);
+            if (c.getIsOpen() || !c.getCanOpen())
+                a.remove(c);
+        }
     }
 
 
 
+
+    /**
+     * Opens a cell
+     */
+    private static Cell openCell(Cell cell) {
+        //open the given cell
+        cell.setIsOpen(true);
+        cell.setCanOpen(false);
+
+        //Set Neighbors' numOpenNeighbors
+        //up
+        changeNumOpenNeighbors(cell.getRow() - 1, cell.getCol());
+        //down
+        changeNumOpenNeighbors(cell.getRow() + 1, cell.getCol());
+        //left
+        changeNumOpenNeighbors(cell.getRow(), cell.getCol() - 1);
+        //right
+        changeNumOpenNeighbors(cell.getRow(), cell.getCol() + 1);
+
+        return cell;
+    }
+
+    private static void changeNumOpenNeighbors(int row, int col) {
+        try {
+            ship[row][col].incNumOpenNeighbors();
+        } catch (ArrayIndexOutOfBoundsException ignore) { }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Initialize the ship
+        ship = new Cell[D][D];
+        for (int r = 0; r < D; r++) {
+            for (int c = 0; c < D; c++) {
+                ship[r][c] = new Cell(r, c);
+            }
+        }
+        makeShip();
+
+        System.out.println("Initial Cell is at row " + initial.getRow() + " and col " + initial.getCol() + "\n\n\n");
+        System.out.println("0123456789");
+        System.out.println();
+
+        //printShip();
+
+    }
+
+    private static void printShip() {
+        for (int r = 0; r < D; r++) {
+            for (int c = 0; c < D; c++) {
+                if (ship[r][c].getIsOpen())
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\n");
     }
 }
