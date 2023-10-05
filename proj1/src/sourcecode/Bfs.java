3373fc7863df52dbf12f4190dbd3ab11bc48650b
patdziedzic <danieldziedzic93@gmail.com>
2023-10-05 17:00:01 -0400

    Once Bot 2 works, 3 is ready

--- proj1/src/sourcecode/Bfs.java
+++ proj1/src/sourcecode/Bfs.java
@@ -57,4 +57,59 @@ public class Bfs {
     private static boolean isValid(Cell c) {
         return (c != null && !c.getOnFire() && c.isOpen && !c.isVisited);
     }
+
+
+    public static LinkedList<Cell> shortestPathBFS_Bot3(Cell bot, Cell button){
+        //make sure isVisited is false for all cells
+        for (int r = 0; r < Ship.D; r++) {
+            for (int c = 0; c < Ship.D; c++) {
+                Ship.ship[r][c].isVisited = false;
+            }
+        }
+
+        Queue<Cell> Q = new LinkedList<>(); //tell us what to explore next
+        HashMap<Cell, Cell> parentNodes = new HashMap<>(); //keeps track of where bot has visited
+        //^ Map the previous to the next by using .put(next, prev)
+        Q.add(bot);
+
+        while (!Q.isEmpty()) {
+            bot = Q.remove();
+            bot.isVisited = true;
+            //shortestPath.add(bot);
+
+            if(bot.equals(button)) {
+                //Once path found, start from end and go back and store the path into LinkedList
+                LinkedList<Cell> shortestPath = new LinkedList<>();
+                Cell ptr = button;
+                while (ptr != null) {
+                    shortestPath.add(ptr);
+                    ptr = parentNodes.get(ptr);
+                }
+                Collections.reverse(shortestPath);
+                return shortestPath;
+            }
+
+            if(isValid(bot.up)) {
+                parentNodes.put(bot.up, bot); //next, previous
+                Q.add(bot.up);
+            }
+            if(isValid(bot.down)) {
+                parentNodes.put(bot.down, bot);
+                Q.add(bot.down);
+            }
+            if(isValid(bot.left)) {
+                parentNodes.put(bot.left, bot);
+                Q.add(bot.left);
+            }
+            if(isValid(bot.right)) {
+                parentNodes.put(bot.right, bot);
+                Q.add(bot.right);
+            }
+        }
+        return null;
+    }
+
+    private static boolean isValid_Bot3(Cell c) {
+        return (c != null && !c.getOnFire() && c.isOpen && !c.isVisited);
+    }
 }
--- proj1/src/sourcecode/Main.java
+++ proj1/src/sourcecode/Main.java
@@ -201,6 +201,95 @@ public class Main {
 
 
     /**
+     * Run an experiment for Bot 3
+     * --> Very similar to bot 2, just changed shortestPath in BFS
+     * @param ship the ship to run the experiment on
+     * @return true if the bot made it to the button
+     */
+    private static boolean runBot3(Cell[][] ship) {
+        //initialize the bot
+        int randIndex = Main.rand(0, openCells.size()-1);
+        Cell bot = openCells.get(randIndex);
+        bot.isBot = true;
+
+        //initialize the button
+        randIndex = Main.rand(0, openCells.size()-1);
+        Cell button = openCells.get(randIndex);
+        button.isButton = true;
+
+        if (bot.isButton) {
+            System.out.println("Bot spawns on button.");
+            return true;
+        }
+
+        //initialize the fire
+        LinkedList<Cell> fireCells = new LinkedList<>();
+        randIndex = Main.rand(0, openCells.size()-1);
+        Cell initialFire = openCells.get(randIndex);
+        initialFire.setOnFire(true); //setting on fire automatically updates neighbors
+        fireCells.add(initialFire);
+
+        if (bot.getOnFire() || button.getOnFire()) {
+            System.out.println("Fire spawns on bot or button.");
+            return false;
+        }
+
+
+        printShip(ship, bot, button, initialFire);
+
+
+        int t = 0;
+        System.out.println(t);
+        while (!bot.isButton && !bot.getOnFire() && !button.getOnFire()) {
+            //BFS Shortest Path from bot -> button
+            LinkedList<Cell> shortestPath;
+
+            //Avoid fire neighbors if possible
+            shortestPath = Bfs.shortestPathBFS_Bot3(bot, button);
+            if (shortestPath == null) {
+                //if not possible, do the Bot 2 method
+                System.out.println("Shortest Path is null.");
+                return false;
+            }
+            shortestPath.removeFirst();
+
+            System.out.println("t = "+ t + " --> @(" + bot.getRow() + ", " + bot.getCol() + ")");
+
+            //move the bot
+            Cell neighbor = shortestPath.removeFirst();
+            bot.isBot = false;
+            neighbor.isBot = true;
+            bot = neighbor;
+
+            if (bot.isButton) {
+                System.out.println("Bot made it to the button!");
+                return true;
+            }
+            else {
+                //else, potentially advance fire
+                LinkedList<Cell> copyFireCells = (LinkedList<Cell>) fireCells.clone();
+                while (!copyFireCells.isEmpty()) {
+                    Cell fireCell = copyFireCells.removeFirst();
+                    tryFireNeighbor(fireCell.up, fireCells);
+                    tryFireNeighbor(fireCell.down, fireCells);
+                    tryFireNeighbor(fireCell.left, fireCells);
+                    tryFireNeighbor(fireCell.right, fireCells);
+                }
+
+                if (bot.getOnFire() || button.getOnFire()) {
+                    System.out.println("The bot or button caught on fire :(");
+                    return false;
+                }
+            }
+            t++;
+        }
+        //if the shortest path is fully traversed and bot didn't reach button, loss
+        System.out.println("Bot never reached button...");
+        return false;
+    }
+
+
+    /**
      * Print the ship
      */
     private static void printShip(Cell[][] ship, Cell bot, Cell button, Cell fire) {
@@ -245,11 +334,12 @@ public class Main {
         //printShip();
 
         //BOT 1
-        //boolean result1 = runBot1(ship);
         //System.out.println("Did Bot 1 make it to the button? " + runBot1(ship));
 
         //BOT 2
-        //boolean result2 = runBot2(ship);
-        System.out.println("Did Bot 2 make it to the button? " + runBot2(ship));
+        //System.out.println("Did Bot 2 make it to the button? " + runBot2(ship));
+
+        //BOT 3
+        System.out.println("Did Bot 3 make it to the button? " + runBot3(ship));
     }
 }
